649a12530f38466fccd81771495e2585be28c538
arsabyaneh <ar.salamy@gmail.com>
2020-04-17 14:58:56 +0200

    designed form layout, added explorer panel

---
+++ Controls/Explorer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoGallery.Controls
+{
+    public class Explorer : TreeView
+    {
+        private ImageList ExplorerImageList;
+
+        private void Initialize()
+        {
+            ExplorerImageList = new ImageList();
+            TreeNode RootNode = new TreeNode();
+
+            this.SuspendLayout();
+
+            ExplorerImageList.Images.Add("MyComputer", Properties.Resources.Computer_16x);
+            ExplorerImageList.Images.Add("Drive", Properties.Resources.HardDrive_16x);
+            ExplorerImageList.Images.Add("CDDrive", Properties.Resources.CDDrive_16x);
+            ExplorerImageList.Images.Add("FolderClosed", Properties.Resources.FolderClosed_16x);
+            ExplorerImageList.Images.Add("FolderOpened", Properties.Resources.FolderOpened_16x);
+
+            this.ImageList = ExplorerImageList;
+            this.ImageIndex = 0;
+            this.SelectedImageIndex = 0;
+            RootNode.Name = "RootNode";
+            RootNode.Text = "My Computer";
+            this.Nodes.Add(RootNode);
+
+            this.ResumeLayout(false);
+        }
+
+        private void AddFolders(string path, TreeNode parent)
+        {
+            string[] directories = null;
+            parent.Nodes.Clear();
+
+            try
+            {
+                if (path == null)
+                    directories = Directory.GetLogicalDrives();
+                else
+                    directories = Directory.GetDirectories(path);
+
+                foreach (string directory in directories)
+                {
+                    TreeNode node = new TreeNode()
+                    {
+                        Text = (path == null) ? directory : Path.GetFileName(directory),
+                        Tag = directory
+                    };
+
+                    if (path == null)
+                    {
+                        node.ImageIndex = node.SelectedImageIndex = (new DriveInfo(directory).DriveType == DriveType.CDRom) ? 2 : 1;
+                    }
+                    else
+                    {
+                        node.ImageIndex = node.SelectedImageIndex = 3;
+                    }
+
+                    parent.Nodes.Add(node);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+
+            if (this.DesignMode)
+                return;
+
+            Initialize();
+            AddFolders(null, this.Nodes[0]);
+            this.Nodes[0].Expand();
+        }
+
+        protected override void OnAfterExpand(TreeViewEventArgs e)
+        {
+            if (e.Node.Level > 1)
+                e.Node.ImageIndex = e.Node.SelectedImageIndex = 4;
+
+            foreach (TreeNode node in e.Node.Nodes)
+            {
+                AddFolders((string)node.Tag, node);
+            }
+
+            base.OnAfterExpand(e);
+        }
+
+        protected override void OnAfterCollapse(TreeViewEventArgs e)
+        {
+            if (e.Node.Level > 1)
+                e.Node.ImageIndex = e.Node.SelectedImageIndex = 3;
+
+            base.OnAfterCollapse(e);
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PhotoGallery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.CurrentDirectoryLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CurrentDirectory = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.explorer1 = new PhotoGallery.Controls.Explorer();
+            this.StatusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.MenuStrip.TabIndex = 0;
+            this.MenuStrip.Text = "MenuStrip";
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CurrentDirectoryLabel,
+            this.CurrentDirectory});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 425);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(800, 25);
+            this.StatusStrip.TabIndex = 1;
+            this.StatusStrip.Text = "statusStrip1";
+            // 
+            // CurrentDirectoryLabel
+            // 
+            this.CurrentDirectoryLabel.Name = "CurrentDirectoryLabel";
+            this.CurrentDirectoryLabel.Size = new System.Drawing.Size(86, 20);
+            this.CurrentDirectoryLabel.Text = "Current Folder:";
+            // 
+            // CurrentDirectory
+            // 
+            this.CurrentDirectory.AutoSize = false;
+            this.CurrentDirectory.Name = "CurrentDirectory";
+            this.CurrentDirectory.Size = new System.Drawing.Size(700, 20);
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 24);
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.Controls.Add(this.explorer1);
+            this.SplitContainer.Size = new System.Drawing.Size(800, 401);
+            this.SplitContainer.SplitterDistance = 222;
+            this.SplitContainer.TabIndex = 2;
+            // 
+            // explorer1
+            // 
+            this.explorer1.Location = new System.Drawing.Point(1, 0);
+            this.explorer1.Name = "explorer1";
+            this.explorer1.Size = new System.Drawing.Size(219, 401);
+            this.explorer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SplitContainer);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.MenuStrip);
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "Form1";
+            this.Text = "Photo Gallery";
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel CurrentDirectoryLabel;
+        private System.Windows.Forms.ToolStripStatusLabel CurrentDirectory;
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private Controls.Explorer explorer1;
+    }
+}
+

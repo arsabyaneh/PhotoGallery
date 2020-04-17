06914e9ec9c68cc2c8fdc2b6006285626704ce2a
arsabyaneh <ar.salamy@gmail.com>
2020-04-18 00:04:47 +0200

    added picture panel and its functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,14 +34,23 @@
             this.CurrentDirectory = new System.Windows.Forms.ToolStripStatusLabel();
             this.SplitContainer = new System.Windows.Forms.SplitContainer();
             this.explorer1 = new PhotoGallery.Controls.Explorer();
+            this.PicturePanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.About = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuStrip.SuspendLayout();
             this.StatusStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
             this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.Panel2.SuspendLayout();
             this.SplitContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // MenuStrip
             // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.HelpToolStripMenuItem});
             this.MenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip.Name = "MenuStrip";
             this.MenuStrip.Size = new System.Drawing.Size(800, 24);
@@ -61,6 +70,7 @@
             // 
             // CurrentDirectoryLabel
             // 
+            this.CurrentDirectoryLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.CurrentDirectoryLabel.Name = "CurrentDirectoryLabel";
             this.CurrentDirectoryLabel.Size = new System.Drawing.Size(86, 20);
             this.CurrentDirectoryLabel.Text = "Current Folder:";
@@ -70,26 +80,70 @@
             this.CurrentDirectory.AutoSize = false;
             this.CurrentDirectory.Name = "CurrentDirectory";
             this.CurrentDirectory.Size = new System.Drawing.Size(700, 20);
+            this.CurrentDirectory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // SplitContainer
             // 
-            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.SplitContainer.Location = new System.Drawing.Point(0, 24);
             this.SplitContainer.Name = "SplitContainer";
             // 
             // SplitContainer.Panel1
             // 
             this.SplitContainer.Panel1.Controls.Add(this.explorer1);
+            // 
+            // SplitContainer.Panel2
+            // 
+            this.SplitContainer.Panel2.Controls.Add(this.PicturePanel);
             this.SplitContainer.Size = new System.Drawing.Size(800, 401);
             this.SplitContainer.SplitterDistance = 222;
             this.SplitContainer.TabIndex = 2;
             // 
             // explorer1
             // 
+            this.explorer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.explorer1.Location = new System.Drawing.Point(1, 0);
             this.explorer1.Name = "explorer1";
             this.explorer1.Size = new System.Drawing.Size(219, 401);
             this.explorer1.TabIndex = 0;
+            this.explorer1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.Explorer_AfterSelect);
+            // 
+            // PicturePanel
+            // 
+            this.PicturePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicturePanel.BackColor = System.Drawing.SystemColors.Window;
+            this.PicturePanel.Location = new System.Drawing.Point(0, 0);
+            this.PicturePanel.Name = "PicturePanel";
+            this.PicturePanel.Size = new System.Drawing.Size(574, 401);
+            this.PicturePanel.TabIndex = 0;
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "&File";
+            // 
+            // HelpToolStripMenuItem
+            // 
+            this.HelpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.About});
+            this.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem";
+            this.HelpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.HelpToolStripMenuItem.Text = "&Help";
+            // 
+            // About
+            // 
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(180, 22);
+            this.About.Text = "&About";
+            this.About.Click += new System.EventHandler(this.About_Click);
             // 
             // Form1
             // 
@@ -101,10 +155,14 @@
             this.Controls.Add(this.MenuStrip);
             this.MainMenuStrip = this.MenuStrip;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Photo Gallery";
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
             this.StatusStrip.ResumeLayout(false);
             this.StatusStrip.PerformLayout();
             this.SplitContainer.Panel1.ResumeLayout(false);
+            this.SplitContainer.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
             this.SplitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -120,6 +178,10 @@
         private System.Windows.Forms.ToolStripStatusLabel CurrentDirectory;
         private System.Windows.Forms.SplitContainer SplitContainer;
         private Controls.Explorer explorer1;
+        private System.Windows.Forms.FlowLayoutPanel PicturePanel;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem About;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,121 @@ namespace PhotoGallery
 {
     public partial class Form1 : Form
     {
+        Panel LastSelectedPicturePanel;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Explorer_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = (string)e.Node.Tag;
+            this.CurrentDirectory.Text = path;
+            try
+            {
+                ShowImageFiles(path);
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void ShowImageFiles(string path)
+        {
+            LastSelectedPicturePanel = null;
+
+            string[] extensions = new[] { "*.jpg", "*.jpeg", "*.bmp", "*.png", "*.gif", "*.tiff" };
+            List<string> ImageFiles = new List<string>();
+
+            foreach (string extension in extensions)
+            {
+                ImageFiles.AddRange(Directory.GetFiles(path, extension));
+            }
+
+            this.PicturePanel.Controls.Clear();
+
+            try
+            {
+                foreach (string imageFile in ImageFiles)
+                {
+                    Image image = Image.FromFile(imageFile);
+
+                    Panel panel = new Panel()
+                    {
+                        Size = new Size(150, 150),
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Tag = imageFile
+                    };
+
+                    Label label = new Label()
+                    {
+                        AutoSize = false,
+                        Text = Path.GetFileName(imageFile),
+                        Height = 20,
+                        Dock = DockStyle.Bottom,
+                        TextAlign = ContentAlignment.MiddleCenter
+                    };
+
+                    PictureBox picture = new PictureBox()
+                    {
+                        Image = image,
+                        Dock = DockStyle.Fill
+                    };
+
+                    if (image.Height > picture.Height || image.Width > picture.Width)
+                        picture.SizeMode = PictureBoxSizeMode.Zoom;
+                    else
+                        picture.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                    panel.Controls.Add(label);
+                    panel.Controls.Add(picture);
+                    this.PicturePanel.Controls.Add(panel);
+
+                    MouseEventHandler mouseDownHandler = new MouseEventHandler(Picture_MouseDown);
+                    panel.MouseDown += mouseDownHandler;
+                    label.MouseDown += mouseDownHandler;
+                    picture.MouseDown += mouseDownHandler;
+
+                    EventHandler mouseDoubleClick = new EventHandler(Picture_DoubleClick);
+                    panel.DoubleClick += mouseDoubleClick;
+                    label.DoubleClick += mouseDoubleClick;
+                    picture.DoubleClick += mouseDoubleClick;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Picture_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (LastSelectedPicturePanel != null)
+            {
+                LastSelectedPicturePanel.BackColor = SystemColors.Window;
+                LastSelectedPicturePanel.ForeColor = SystemColors.ControlText;
+            }
+
+            Panel selectedPanel = (Panel) (sender is Panel ? sender : (sender as Control).Parent);
+            selectedPanel.BackColor = SystemColors.Highlight;
+            selectedPanel.ForeColor = SystemColors.HighlightText;
+            LastSelectedPicturePanel = selectedPanel;
+        }
+
+        private void Picture_DoubleClick(object sender, EventArgs e)
+        {
+            if (LastSelectedPicturePanel == null)
+                return;
+
+            string path = (string)LastSelectedPicturePanel.Tag;
+            System.Diagnostics.Process.Start(path);
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Photo Gallery, All Rights Reserved 2020.", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
